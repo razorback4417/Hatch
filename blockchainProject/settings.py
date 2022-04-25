36ad99da634f40e7027fef43751b699d4b2b34a5
Theo Luu <44447723+razorback4417@users.noreply.github.com>
2022-04-24 21:58:50 -0400

    Update settings.py

--- blockchainProject/settings.py
+++ blockchainProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u$ync^r-!ffn9)q83eu7-a22ggt(3!8=#ltjy4bxg1@8t2%was'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
